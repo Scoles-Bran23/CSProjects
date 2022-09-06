e6faa21193edea69207ea3a128802bfaf00586da
Katie SB <katescoles-bran23@marlborough.org>
2022-09-06 14:04:58 -0700

    new files

---
+++ IntroTojsp5/hellop5/main.js
@@ -0,0 +1,33 @@
+let x, w;
+function setup(){
+  x = 0;
+  w = 10;
+  createCanvas(400, 400);
+}
+function draw(){
+  background(255);
+
+
+  /*
+  if(mouseX > width/2){
+    x++;
+  }
+  else{
+    x--;
+  }*/
+
+  if(mouseY > width/2){
+    w++;
+    fill(235, 52, 119);
+  }
+  else{
+    w--;
+    fill(50, 129, 168);
+  }
+
+  noStroke();
+  ellipse(width/2, width/2, w, w);
+  stroke(0);
+  strokeWeight(w/8);
+  ellipse(width/2, width/2, w/4, w/4);
+}
